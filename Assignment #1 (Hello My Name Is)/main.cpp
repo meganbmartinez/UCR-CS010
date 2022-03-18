211e91113de814ad1535c0009b4567a268eb0e01
meganbmartinez <mmart085@ucr.edu>
2022-03-17 18:38:57 -0700

    Uploaded Assignment #1 (Hello My Name Is)

---
+++ Assignment #1 (Hello My Name Is)/main.cpp
@@ -0,0 +1,26 @@
+// Course: CS 10 Fall 2013
+//
+// First Name: Megan
+// Last Name: Martinez
+// Course Username: <mmart085@ucr.edu>
+// Lecture Section: <001>
+// Lab Section: <029>
+// TA: Malcolm Mumme
+//
+// Assignment: <assn1>
+//
+// I hereby certify that the code in this file
+// is ENTIRELY my own original work.
+//
+// Megan Brittany Martinez
+// =================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string myName = "Megan Brittany Martinez";
+	cout << "Hello! My name is " << myName << ".\n";
+	return 0;
+}
